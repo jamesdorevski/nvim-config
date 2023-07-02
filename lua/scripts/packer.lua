7eefc96643221563c29054421db8cd22876a4686
James Dorevski <github@jamesdorevski.com>
2023-07-02 12:24:06 +1000

    using tokyonight colourscheme

--- lua/scripts/packer.lua
+++ lua/scripts/packer.lua
@@ -7,13 +7,12 @@ return require('packer').startup(function(use)
 	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-
-  use({ 
-	  'rose-pine/neovim', 
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
+ 
+  use({
+    "folke/tokyonight.nvim",
+    config = function()
+        vim.cmd("colorscheme tokyonight-storm")
+    end
   })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
