7f615dd8988ebe4b23c954804a9a8772fc75cc11
James Dorevski <github@jamesdorevski.com>
2023-07-12 14:32:35 +1000

    removed some cmp nonsense

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,6 +9,25 @@ lsp.ensure_installed({
     "csharp_ls"
 })
 
+-- Fix Undefined global 'vim'
+lsp.nvim_workspace()
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
 lsp.set_preferences({
     sign_icons = {
         error = 'E',
