3d5522f43e2004e6cc0e29cc486fa22eef1eff07
James Dorevski <github@jamesdorevski.com>
2023-07-18 07:56:26 +1000

    added remaps for tabs

--- lua/scripts/remap.lua
+++ lua/scripts/remap.lua
@@ -31,9 +31,13 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
-
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+-- Better tab experience
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>")
+vim.keymap.set("n", "<leader>t<leader>", "<cmd>tabnext<CR>")
+vim.keymap.set("n", "<leader>tm", "<cmdtabmove<CR>")
+vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>")
+vim.keymap.set("n", "<leader>to", "<cmd>tabonly<CR>")
