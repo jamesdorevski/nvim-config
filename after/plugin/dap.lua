9dc0920f232c61d4bd5ed34b2cbb403d6b62223a
James Dorevski <github@jamesdorevski.com>
2023-08-02 20:44:25 +1000

    stole dap shortcuts from docs

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,10 +1,29 @@
 local dap = require('dap')
 
-vim.keymap.set("n", "<leader>bp", function() dap.toggle_breakpoint() end)
-vim.keymap.set("n", "<leader>db", function() dap.continue() end)
-vim.keymap.set("n", "<leader>dn", function() dap.step_over() end)
-vim.keymap.set("n", "<leader>di", function() dap.step_into() end)
-vim.keymap.set("n", "<leader>ds", function() dap.repl_open() end)
+vim.keymap.set('n', '<F5>', function() dap.continue() end)
+vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+vim.keymap.set('n', '<F12>', function() dap.step_out() end)
+vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>B', function() dap.set_breakpoint() end)
+vim.keymap.set('n', '<Leader>lp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
+vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+    require('dap.ui.widgets').hover()
+end)
+vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+    require('dap.ui.widgets').preview()
+end)
+vim.keymap.set('n', '<Leader>df', function()
+    local widgets = require('dap.ui.widgets')
+    widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+    local widgets = require('dap.ui.widgets')
+    widgets.centered_float(widgets.scopes)
+end)
+
 
 dap.adapters.python = function(cb, config)
   if config.request == 'attach' then
@@ -23,7 +42,7 @@ dap.adapters.python = function(cb, config)
   else
     cb({
       type = 'executable',
-      command = 'home/james/.virtualenvs/debugpy/bin/python',
+      command = '/home/james/.virtualenvs/debugpy/bin/python',
       args = { '-m', 'debugpy.adapter' },
       options = {
         source_filetype = 'python',
