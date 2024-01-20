124adf3ad2588163d98147dd0e882da877bc475e
James Dorevski <github@jamesdorevski.com>
2024-01-20 13:31:48 +1100

    removed unused lsp

--- after/plugin/floaterm.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', "<leader>ft", ":FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=2 fish <CR> ")
-vim.keymap.set('n', "t", ":FloatermToggle myfloat<CR>")
-vim.keymap.set('t', "<Esc>", "<C-\\><C-n>:q<CR>")
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,8 +3,7 @@ local lsp = require('lsp-zero')
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-	"rust_analyzer",
-    "lua-language-server"
+	"rust_analyzer"
 })
 
 -- Fix Undefined global 'vim'
